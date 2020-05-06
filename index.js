6dfd05efaadccc01fdd2a073e50cd73fbdb5d68a
Max Lamb <lordmaxlamb@gmail.com>
2020-05-05 18:12:30 -0700

    some more work done

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 //import express from 'express';
+const dotenv = require('dotenv');
+
+dotenv.config();
 const express = require('express');
+const cors = require('cors');
+
+const data = require('./data/weather.json');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+// const express = require('express');
 
 const app = express();
 
 app.get('/location', (req, res) => {
     res.json({
-        formatted_query: '',
-        latitude: '',
-        longitude: '',
-    });
+        "formatted_query": "Seattle, WA, USA",
+        "latitude": "47.606210",
+        "longitude": "-122.332071"
 });
 
-app.listen(PORT, () => { console.log('listening on PORT')};
+app.listen(PORT, () => { console.log('listening on PORT')});
